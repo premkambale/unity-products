8c58892b16ac92ec1b869945cd4f2cd28acdc9ca
Hrishikesh Sonawane <102814156+Hrishikeshsonawane@users.noreply.github.com>
2023-11-08 19:55:39 +0530

    chnags

--- frontend/src/Components/Main/Admin/Analytics/Analytics.jsx
+++ frontend/src/Components/Main/Admin/Analytics/Analytics.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import "./Analytics.css"
 import Table from 'react-bootstrap/Table';
+import { Badge } from 'react-bootstrap';
+import { MdDelete, MdModeEditOutline } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
 const Analytics = () => {
     const [activeTab, setActiveTab] = useState('table');
@@ -32,334 +35,83 @@ const Analytics = () => {
     }
 
 
+    const navigate = useNavigate()
 
-    const handleTabClick = (tabName) => {
-        setActiveTab(tabName);
+
+
+
+    const handleEdit = () => {
+        navigate("/Admin/Admin/EditProduct")
     }
 
-
-
-
-    const data = [
-        {
-            value: '278',
-            title: 'Active Products',
-        },
-        {
-            value: '156',
-            title: 'inventory products',
-        },
-        {
-            value: '64.89 %',
-            title: 'Bounce Rate',
-        },
-        {
-            value: '423',
-            title: 'Total Visits',
-        },
-        {
-            value: '278',
-            title: 'New Posts',
-        },
-        {
-            value: '156',
-            title: 'New Comments',
-        },
-        {
-            value: '64.89 %',
-            title: 'Bounce Rate',
-        },
-        {
-            value: '423',
-            title: 'Total Visits',
-        },
-    ];
-
-    const products = [
-        {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }, {
-            "Sr_no": "1",
-            "product_name": "Prescription Glasses",
-            "category": "glassess",
-            "company_name": "Warby Parker",
-            "price": "$199.99",
-            "ratings": "4.5",
-            "description": "Customizable prescription glasses for clear vision",
-            "image": "0000111100000111",
-            "quantity": 2
-        }
-    ]
-
-
-    const handleEdit=()=>{
-        alert("edit")
-    }
-
-    const handleDelete=()=>{
-        alert("delete")
+    const handleDelete = () => {
+        alert("product deleted")
     }
 
     return (
         <div className="analyticsContainer">
-            <div className="AnalyticsTabs">
-                <div className="TabButtons">
-                    <button
-                        onClick={() => handleTabClick('table')}
-                        className={activeTab === 'table' ? 'active' : ''}
-                    >
-                        Table
-                    </button>
-                    <button
-                        onClick={() => handleTabClick('card')}
-                        className={activeTab === 'card' ? 'active' : ''}
-                    >
-                        Card
-                    </button>
-                    <button
-                        onClick={() => handleTabClick('item')}
-                        className={activeTab === 'item' ? 'active' : ''}
-                    >
-                        Item
-                    </button>                </div>
-                <div className="TabContent">
-                    {activeTab === 'table' && (
-                        <div className="AnaProductTable">
-                            <div className="table-scroll">
-                                <Table responsive="md" scroll borderless bordered hover>
-                                    <thead style={{ marginTop: "2vh" }}>
-                                        <tr>
-                                            <th className='DashTableHead'>Sr.No</th>
-                                            <th className='DashTableHead'>product_name</th>
-                                            <th className='DashTableHead'>category</th>
-                                            <th className='DashTableHead'>company_name</th>
-                                            <th className='DashTableHead'>price</th>
-                                            <th className='DashTableHead'>description</th>
-                                            <th className='DashTableHead'>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className='tbodyDash' style={{ fontSize: "0.9rem", fontFamily: "revert", textAlign: "start" }}>
-                                        {ProductData.map((product, index) => (
-                                            <tr key={index} className={index % 2 === 0 ? 'even' : 'odd'}>
-                                                <td>{index + 1}</td>
-                                                <td>{product.product_name}</td>
-                                                <td>{product.product_category}</td>
-                                                <td>{product.company_name}</td>
-                                                <td>{product.product_price}</td>
-                                                <td>{product.product_description}</td>
-                                                <td className="DashboardAction">
-                                                    <button onClick={handleEdit}>
-                                                        edit                                                    </button>
-                                                    <button onClick={() => handleDelete(product._id)}>
-                                                        delete                        </button>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </Table>
-                            </div>
-                        </div>
-                    )}
-                    {activeTab === 'card' && (
-                        <div className="AnaProductCard">
-                            <div className="grey-bg container-fluid">
-
-                                <section id="minimal-statistics">
-
-                                    <div className="row">
-                                        {data.map((item, index) => (
-                                            <div key={index} className="col-xl-3 col-sm-6 col-12">
-                                                <div style={{ backgroundColor: "#463b4c", color: "red" }} className="card">
-                                                    <div className="card-content">
-                                                        <div className="card-body">
-                                                            <div className="media d-flex">
-                                                                <div className="align-self-center">
-                                                                    <i className={`icon-${item.icon} ${item.color} font-large-2 float-left`}></i>
-                                                                </div>
-                                                                <div className="media-body text-right">
-                                                                    <h3>{item.value}</h3>
-                                                                    <span style={{ color: "white" }}>{item.title}</span>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </section>
-
-
-                            </div>
-                        </div>
-                    )}
-                    {activeTab === 'item' && (
-                        <div className="AnaProductItem">
-
-
-                        </div>
-                    )}
-
-                </div>
+            <div className="analyticsTitle">
+                <span>Analytics</span>
             </div>
+            <div className="anaTabs">
+                <div className="tab1">
+                    Online products                </div>
+                <div className="tab1">
+                    Inventory
+                </div>
+                <div className="tab1">
+                    tab1
+                </div>
+                <div className="tab1">
+                    tab1
+                </div>
+
+            </div>
+
+            <div className="anatable">
+                <span className="tableTitleAna">
+                    Inventory Table
+                </span>
+
+                <div style={{ width: "100%" }} className="table-scroll">
+                    <Table responsive="md" scroll borderless bordered hover>
+                        <thead style={{ marginTop: "2vh" }}>
+                            <tr>
+                                <th className='DashTableHead'>Sr.No</th>
+                                <th className='DashTableHead'>product_name</th>
+                                <th className='DashTableHead'>category</th>
+                                <th className='DashTableHead'>company_name</th>
+                                <th className='DashTableHead'>price</th>
+                                <th className='DashTableHead'>description</th>
+                                <th className='DashTableHead'>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody className='tbodyDash' style={{ fontSize: "0.9rem", fontFamily: "revert", textAlign: "start" }}>
+                            {ProductData.map((product, index) => (
+                                <tr key={index} className={index % 2 === 0 ? 'even' : 'odd'}>
+                                    <td>{index + 1}</td>
+                                    <td>{product.product_name}</td>
+                                    <td>{product.product_category}</td>
+                                    <td>{product.company_name}</td>
+                                    <td>{product.product_price}</td>
+                                    <td>{product.product_description}</td>
+                                    <td className="DashboardAction">
+                                        <button onClick={handleEdit}>
+                                            <MdModeEditOutline />
+                                        </button>
+                                        <button onClick={() => handleDelete(product._id)}>
+                                            <MdDelete />    
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
+
+            </div>
+
+
         </div>
     )
 }
--- frontend/src/Components/Main/Products/Products.jsx
+++ frontend/src/Components/Main/Products/Products.jsx
@@ -5,25 +5,30 @@ import "./Products.css"
 import OurProducts from '../OurProducts/OurProducts'
 import Header from '../Layout/Header/Header';
 import Footer from '../Layout/Header/Footer';
+import { useNavigate } from 'react-router-dom';
+import { BiHome } from 'react-icons/bi';
 const Products = () => {
+
+const navigate = useNavigate()
+
   const breadcrumbItems = [
     { label: 'Home', link: '/' },
     { label: 'Our Products' },
-];
+  ];
   return (
     <>
-    <Header />
-    <div className='UnityWhyTab'>
-                    <Breadcrumb>
-                        {breadcrumbItems.map((item, index) => (
-                            <Breadcrumb.Item key={index} href={item.link}>{item.label}</Breadcrumb.Item>
-                        ))}
-                    </Breadcrumb>
-                    <h1 className='headingOfHistory' style={{ textAlign: "center" }}>  Our Products</h1>
-                </div>
-       <OurProducts />
- <Footer />
- </>
+      <Header />
+      <div className='UnityWhyTab'>
+        <div className="breadCrump">
+          <span onClick={() => navigate("/Home")} className='breadItems'> <BiHome fontSize='13px' /> Home &gt;
+          </span>
+          <span onClick={() => navigate("/OurHistory")} className='breadItems2'>Our History</span>
+        </div>
+        <h1 className='headingOfHistory' style={{ textAlign: "center" }}>  Our Products</h1>
+      </div>
+      <OurProducts />
+      <Footer />
+    </>
   )
 }
 
